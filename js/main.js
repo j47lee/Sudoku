62d392e31de1cb7e772b4fa90e0e12c95003a96a
Jonathan Lee <j47lee@gmail.com>
2015-07-09 15:47:39 -0700

    Initial setup divs made undroppable

--- js/main.js
+++ js/main.js
@@ -1,32 +1,5 @@
 $(function(){
 
-
-// board = [
-//   [null, null, 4, 6, null, 8, 9, 1, 2],
-//   [null, 7, 2, null, null, null, 3, 4, 8],
-//   [1, null, null, 3, 4, 2, 5, null, 7],
-//   [null, 5, 9, 7, null, 1, 4, 2, null],
-//   [null, 2, 6, null, 5, null, 7, 9, null],
-//   [null, 1, 3, 9, 2, 4, 8, 5, null],
-//   [9, null, 1, 5, 3, 7, null, null, 4],
-//   [2, 8,	7, null, null, null, 6, 3, null],
-//   [3, 4, 5, 2, null, 6, 1, null, null],
-// ]
-
-// board = [
-//   [' ', ' ', 4, 6, ' ', 8, 9, 1, 2],
-//   [' ', 7, 2, ' ', ' ', ' ', 3, 4, 8],
-//   [1, ' ', ' ', 3, 4, 2, 5, ' ', 7],
-//   [' ', 5, 9, 7, ' ', 1, 4, 2, ' '],
-//   [' ', 2, 6, ' ', 5, ' ', 7, 9, ' '],
-//   [' ', 1, 3, 9, 2, 4, 8, 5, ' '],
-//   [9, ' ', 1, 5, 3, 7, ' ', ' ', 4],
-//   [2, 8,	7, ' ', ' ', ' ', 6, 3, ' '],
-//   [3, 4, 5, 2, ' ', 6, 1, ' ', ' '],
-// ]
-
-//board with zeros for null, need to figure out how to use null or have empty values!!
-//might be conflict with the CHECKS!!
 board = [
   [0, 0, 4, 6, 0, 8, 9, 1, 2],
   [0, 7, 2, 0, 0, 0, 3, 4, 8],
@@ -39,7 +12,7 @@ board = [
   [3, 4, 5, 2, 0, 6, 1, 0, 0],
 ]
 
-//solved
+//SOLUTION
 // board = [
 // [5, 3, 4, 6, 7, 8, 9, 1, 2],
 // [6, 7, 2, 1, 9, 5, 3, 4, 8],
@@ -52,13 +25,12 @@ board = [
 // [3, 4, 5, 2, 8, 6, 1, 7, 9]
 // ]
 
-
 ///////////////////////POPULATES HTML BOARD FROM ARRAY////////////////////
 var squares = document.getElementsByClassName('square'); //generates array from divs
 for (var i = 0; i < board.length; i++) {
   for (var j = 0; j < board[i].length; j++) {
     var squareNumber = j+(i*board[i].length);
-    if(board[i][j]){
+    if (board[i][j]) {
       squares[squareNumber].innerHTML = board[i][j];
     } else {
       console.log('Square is 0. Do not fill')
@@ -70,154 +42,176 @@ for (var i = 0; i < board.length; i++) {
 //Initiates draggable numbers into each row
 $('.number').draggable({helper:'clone'});
 
-
-            for (var i = 0; i < board.length; i++) {
-              for (var j = 0; j < board[i].length; j++) {
-                if (board[i][j]<1) {
-                // console.log(board[i][j]);
-
-                } else {
-
-                }
-              }
-            }
-
-
-
-
 //for row1
 $('.row1').children('div').each(function() {
-  $(this).droppable({          // "this" refers to the div
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[0][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({          // "this" refers to the div
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[0][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
+
 
 //for row2
 $('.row2').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[1][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[1][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 //for row3
 $('.row3').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[2][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[2][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 //for row4
 $('.row4').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[3][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[3][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 //for row5
 $('.row5').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[4][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[4][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 //for row6
 $('.row6').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[5][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[5][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 //for row7
 $('.row7').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[6][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[6][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 //for row8
 $('.row8').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[7][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[7][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 //for row9
 $('.row9').children('div').each(function() {
-  $(this).droppable({
-    drop: function(ev,ui){
-      var draggedNumber = $(ui.draggable).html();
-      $(this).html(draggedNumber);
-      for (var i = 0; i < 9; i++) {
-        if (parseInt(this.title) == i) {
-          board[8][i] = parseInt(this.innerHTML);
+  if (this.innerHTML == 0) {
+    $(this).droppable({
+      drop: function(ev,ui){
+        var draggedNumber = $(ui.draggable).html();
+        $(this).html(draggedNumber);
+        for (var i = 0; i < 9; i++) {
+          if (parseInt(this.title) == i) {
+            board[8][i] = parseInt(this.innerHTML);
+          }
         }
       }
-    }
-  })
+    })
+  } else {
+    $(this).css('background-color','red')
+  }
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////
