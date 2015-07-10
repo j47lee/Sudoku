0b0de0e33a498fd11ed6dc264e64741cc7c9ef09
Jonathan Lee <j47lee@gmail.com>
2015-07-09 23:09:14 -0700

    Clean up code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 
+//PUZZLE
 board = [
   [0, 0, 4, 6, 0, 8, 9, 1, 2],
   [0, 7, 2, 0, 0, 0, 3, 4, 8],
@@ -25,8 +26,8 @@ board = [
 // [3, 4, 5, 2, 8, 6, 1, 7, 9]
 // ]
 
-///////////////////////POPULATES HTML BOARD FROM ARRAY////////////////////
-var squares = document.getElementsByClassName('square'); //generates array from divs
+//////////////////////////POPULATES HTML BOARD FROM ARRAY///////////////////////
+var squares = document.getElementsByClassName('square');
 
 function setBoard(){
   var boardReset = [
@@ -55,11 +56,12 @@ function setBoard(){
 
 setBoard();
 
-////////////////////////////////DRAG AND DROP//////////////////////////////////
-//Initiates draggable numbers into each row
+////////////////////////////////DRAG AND DROP///////////////////////////////////
+//Creates Draggable numbers
 $('.number').draggable({helper:'clone'});
 
-//for row1
+//Creates Droppable rows
+//Row1
 $('.row1').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({          // "this" refers to the div
@@ -78,7 +80,7 @@ $('.row1').children('div').each(function() {
   }
 });
 
-//for row2
+//Row2
 $('.row2').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -97,7 +99,7 @@ $('.row2').children('div').each(function() {
   }
 });
 
-//for row3
+//Row3
 $('.row3').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -116,7 +118,7 @@ $('.row3').children('div').each(function() {
   }
 });
 
-//for row4
+//Row4
 $('.row4').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -135,7 +137,7 @@ $('.row4').children('div').each(function() {
   }
 });
 
-//for row5
+//Row5
 $('.row5').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -154,7 +156,7 @@ $('.row5').children('div').each(function() {
   }
 });
 
-//for row6
+//Row6
 $('.row6').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -173,7 +175,7 @@ $('.row6').children('div').each(function() {
   }
 });
 
-//for row7
+//Row8
 $('.row7').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -192,7 +194,7 @@ $('.row7').children('div').each(function() {
   }
 });
 
-//for row8
+//Row8
 $('.row8').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -211,7 +213,7 @@ $('.row8').children('div').each(function() {
   }
 });
 
-//for row9
+//Row9
 $('.row9').children('div').each(function() {
   if (this.innerHTML == 0) {
     $(this).droppable({
@@ -230,65 +232,60 @@ $('.row9').children('div').each(function() {
   }
 });
 
-////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////ALL WRAPPED IN CHECK BUTTON////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////WRAPPED IN CHECK BUTTON/////////////////////////
 $('#check').click(function(){
 
-//////////////////////////////////ROWS CHECK////////////////////////////////
+//////////////////////////////////ROWS CHECKER//////////////////////////////////
 var allRows45;
 for (var i = 0; i < board.length; i++) {
   var sumCurrentRow = eval(board[i].join('+'));
-  // console.log(sumCurrentRow);
   if (sumCurrentRow === 45) {
     allRows45 = true;
-    // console.log('row ' + (i+1) + ' is equal to 45');
-    console.log((typeof allRows45) + '=' + allRows45 + ' All Rows are correct');
   } else {
     allRows45 = false;
-    console.log((typeof allRows45) + '=' + allRows45 + ' Not all Rows are correct');
-    // console.log(allRows45 + ' Not all rows are correct');
     break;
   }
 }
 
-//////////////////////////////////COLUMN CHECK////////////////////////////////
+////////////////////////////////COLUMNS CHECKER/////////////////////////////////
 var col1 = []; var col2 = []; var col3 = [];
 var col4 = []; var col5 = []; var col6 = [];
 var col7 = []; var col8 = []; var col9 = [];
 
-//column1 check
+//column1
 for (var i = 0; i < board.length; i++) {
   col1.push(board[i][0]);
 }
-//column2 check
+//column2
 for (var i = 0; i < board.length; i++) {
   col2.push(board[i][1]);
 }
-//column3 check
+//column3
 for (var i = 0; i < board.length; i++) {
   col3.push(board[i][2]);
 }
-//column4 check
+//column4
 for (var i = 0; i < board.length; i++) {
   col4.push(board[i][3]);
 }
-//column5 check
+//column5
 for (var i = 0; i < board.length; i++) {
   col5.push(board[i][4]);
 }
-//column6 check
+//column6
 for (var i = 0; i < board.length; i++) {
   col6.push(board[i][5]);
 }
-//column7 check
+//column7
 for (var i = 0; i < board.length; i++) {
   col7.push(board[i][6]);
 }
-//column8 check
+//column8
 for (var i = 0; i < board.length; i++) {
   col8.push(board[i][7]);
 }
-//column9 check
+//column9
 for (var i = 0; i < board.length; i++) {
   col9.push(board[i][8]);
 }
@@ -300,18 +297,16 @@ var ec7 = eval(col7.join('+')); var ec8 = eval(col8.join('+')); var ec9 = eval(c
 var allCols45;
 if (ec1===ec2 && ec2===ec3 && ec3===ec4 && ec4===ec5 && ec5===ec6 && ec6===ec7 && ec7===ec8 && ec8===ec9 && ec9===45) {
   allCols45 = true;
-  console.log((typeof allCols45) + '=' + allCols45 + ' All columns are correct');
 } else {
   allCols45 = false;
-  console.log((typeof allCols45) + '=' + allCols45 + ' Not all columns are correct');
 }
 
-/////////////////////////////////3X3 BOXES CHECK////////////////////////////////
+///////////////////////////////3X3 BOXES CHECKER////////////////////////////////
 var firstBox = []; var secondBox = []; var thirdBox = [];
 var fourthBox = []; var fifthBox = []; var sixthBox = [];
 var seventhBox = []; var eighthBox = []; var ninthBox = [];
 
-//check 1-3 boxes
+//1-3 boxes
 for (var i = 0; i < 3; i++) {
   for (var j = 0; j < 3; j++) {
     firstBox.push(board[i][j]);
@@ -323,7 +318,7 @@ for (var i = 0; i < 3; i++) {
     thirdBox.push(board[i][j]);
   }
 }
-//check 4-6 boxes
+//4-6 boxes
 for (var i = 3; i < 6; i++) {
   for (var j = 0; j < 3; j++) {
     fourthBox.push(board[i][j]);
@@ -335,7 +330,7 @@ for (var i = 3; i < 6; i++) {
     sixthBox.push(board[i][j]);
   }
 }
-//check 7-9 boxes
+//7-9 boxes
 for (var i = 6; i < 9; i++) {
   for (var j = 0; j < 3; j++) {
     seventhBox.push(board[i][j]);
@@ -355,15 +350,13 @@ var eb7 = eval(seventhBox.join('+')); var eb8 = eval(eighthBox.join('+')); var e
 var allBoxes45;
 if (eb1===eb2 && eb2===eb3 && eb3===eb4 && eb4===eb5 && eb5===eb6 && eb6===eb7 && eb7===eb8 && eb8===eb9 && eb9===45) {
   allBoxes45 = true;
-  console.log((typeof allBoxes45) + '=' + allBoxes45 + ' All Boxes are correct');
 } else {
   allBoxes45 = false;
-  console.log((typeof allBoxes45) + '=' + allBoxes45 + ' Not all Boxes are correct');
 }
 
-//////////////////////////////////END OF BUTTON, SET ALERT/////////////////////////////////
+///////////////////////////////END OF CHECK BUTTON//////////////////////////////
   if (allRows45 && allCols45 && allBoxes45) {
-    alert('You solved the puzzle!')
+    alert("You've solved the puzzle!")
     $('#scoreSec').html($('#seconds').html());
     $('#scoreMin').html($('#minutes').html());
     sec = 0;
@@ -372,13 +365,13 @@ if (eb1===eb2 && eb2===eb3 && eb3===eb4 && eb4===eb5 && eb5===eb6 && eb6===eb7 &
   }
 });
 
-/////////////////////////////////////GAME AND TIMER RESET///////////////////////////////////////
+///////////////////////////////GAME AND TIMER RESET/////////////////////////////
 $('#reset').click(function(){
   setBoard();
   sec = 0;
 });
 
-////////////////////////////////////////TIMER//////////////////////////////////////////////
+/////////////////////////////////////TIMER//////////////////////////////////////
 var sec = 0;
 function pad ( val ) { return val > 9 ? val : "0" + val; }
 setInterval( function(){
@@ -386,7 +379,7 @@ setInterval( function(){
   $('#minutes').html(pad(parseInt(sec/60,10)));
 }, 1000);
 
-//////////////////////////CHECK AND RESET BUTTON ANIMATION///////////////////////////////
+//////////////////////CHECK AND RESET BUTTON ANIMATION//////////////////////////
 
 $('#check').mouseover(function() {
   $(this).animate({ backgroundColor: 'green', color: 'white' }, 500); }).mouseout(function() {
@@ -395,6 +388,7 @@ $('#check').mouseover(function() {
 $('#reset').mouseover(function() {
   $(this).animate({ backgroundColor: 'red', color: 'white' }, 500); }).mouseout(function() {
     $(this).animate({ backgroundColor: 'white', color: 'red' }, 200); })
+
 
 
 }) //end of jq
